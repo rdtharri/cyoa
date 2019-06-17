28794a61ef9a66fb46ecc276efa365e08287f589
Ricky Tharrington <Ricky.Tharrington@sas.com>
2019-06-17 13:42:38 -0400

    Adding functional options

--- story.go
+++ story.go
@@ -28,36 +28,102 @@ var DefaultHandlerTmpl = `
     <title>Choose Your Own Adventure!</title>
   </head>
   <body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-    <p>{{.}}</p>
-    {{end}}
-    <ul>
-      {{range .Options}}
-        <li><a href="/{{.Chapter}}" >{{.Text}}</a></li>
-      {{end}}
-    </ul>
+		<section class="page">
+	    <h1>{{.Title}}</h1>
+	    {{range .Paragraphs}}
+	    <p>{{.}}</p>
+	    {{end}}
+	    <ul>
+	      {{range .Options}}
+	        <li><a href="/{{.Chapter}}" >{{.Text}}</a></li>
+	      {{end}}
+	    </ul>
+		</section>
+		<style>
+		      body {
+		        font-family: helvetica, arial;
+		      }
+		      h1 {
+		        text-align:center;
+		        position:relative;
+		      }
+		      .page {
+		        width: 80%;
+		        max-width: 500px;
+		        margin: auto;
+		        margin-top: 40px;
+		        margin-bottom: 40px;
+		        padding: 80px;
+		        background: #FCF6FC;
+		        border: 1px solid #eee;
+		        box-shadow: 0 10px 6px -6px #797;
+		      }
+		      ul {
+		        border-top: 1px dotted #ccc;
+		        padding: 10px 0 0 0;
+		        -webkit-padding-start: 0;
+		      }
+		      li {
+		        padding-top: 10px;
+		      }
+		      a,
+		      a:visited {
+		        text-decoration: underline;
+		        color: #555;
+		      }
+		      a:active,
+		      a:hover {
+		        color: #222;
+		      }
+		      p {
+		        text-indent: 1em;
+		      }
+		    </style>
   </body>
 </html>
 `
 
-func NewHandler(s Story) http.Handler {
-	return handler{s}
+type HandlerOption func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
 }
 
 type handler struct {
-	s Story
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
-	path = path[1:]
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 
 	if chapter, ok := h.s[path]; ok {
-		err := tpl.Execute(w, chapter)
+		err := h.t.Execute(w, chapter)
 		if err != nil {
 			log.Printf("%v", err)
 			http.Error(w, MapKeyError, http.StatusInternalServerError)
